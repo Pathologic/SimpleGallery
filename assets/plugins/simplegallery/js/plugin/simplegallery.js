3be5f22c03f03c21ba7bc331cd99ecafbd405fa5
Pathologic <maxx@np.by>
2014-11-30 15:14:01 +0300

    fix https://github.com/Pathologic/SimpleGallery/issues/41

--- assets/plugins/simplegallery/js/plugin/simplegallery.js
+++ assets/plugins/simplegallery/js/plugin/simplegallery.js
@@ -363,11 +363,11 @@ var sgHelper = {};
     		});
 		},
 		bytesToSize: function(bytes) {
-		   if(bytes == 0) return '0 байт';
+		   if(bytes == 0) return '0 B';
 		   var k = 1024;
 		   var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 		   var i = Math.floor(Math.log(bytes) / Math.log(k));
-		   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+		   return (bytes / Math.pow(k, i)).toFixed(0) + ' ' + sizes[i];
 		},
 		stripText: function(str,len) {
 			str.replace(/<\/?[^>]+>/gi, '');
