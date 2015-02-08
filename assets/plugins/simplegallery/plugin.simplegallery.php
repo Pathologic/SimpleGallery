1898cdcbf5c8396801c33ea821c1af7e3d1de166
Pathologic <maxx@np.by>
2015-02-09 01:32:21 +0300

    small refactor

--- assets/plugins/simplegallery/plugin.simplegallery.php
+++ assets/plugins/simplegallery/plugin.simplegallery.php
@@ -4,19 +4,18 @@ $e = &$modx->event;
 if ($e->name == 'OnDocFormRender' && $id) {
     include_once(MODX_BASE_PATH . 'assets/plugins/simplegallery/lib/plugin.class.php');
     global $modx_lang_attribute;
-    $simpleGallery = new \SimpleGallery\sgPlugin($modx, $modx_lang_attribute);
-    $output = $simpleGallery->render();
+    $plugin = new \SimpleGallery\sgPlugin($modx, $modx_lang_attribute);
+    $output = $plugin->render();
     if ($output) $e->output($output);
 }
 if ($e->name == 'OnEmptyTrash') {
     if (empty($ids)) return;
-    $where = implode(',', $ids);
-    $modx->db->delete($modx->getFullTableName("sg_images"), "`sg_rid` IN ($where)");
     include_once(MODX_BASE_PATH . 'assets/plugins/simplegallery/lib/plugin.class.php');
-    $simpleGallery = new \SimpleGallery\sgPlugin($modx);
-    $simpleGallery->clearFolders($ids, MODX_BASE_PATH . $e->params['thumbsCache'] . $e->params['folder']);
-    $simpleGallery->clearFolders($ids, MODX_BASE_PATH . $e->params['folder']);
-    $sql = "ALTER TABLE {$modx->getFullTableName('sg_images')} AUTO_INCREMENT = 1";
+    $plugin = new \SimpleGallery\sgPlugin($modx);
+    $where = implode(',', $ids);
+    $modx->db->delete($plugin->_table, "`sg_rid` IN ($where)");
+    $plugin->clearFolders($ids, MODX_BASE_PATH . $e->params['thumbsCache'] . $e->params['folder']);
+    $plugin->clearFolders($ids, MODX_BASE_PATH . $e->params['folder']);
+    $sql = "ALTER TABLE {$plugin->_table} AUTO_INCREMENT = 1";
     $rows = $modx->db->query($sql);
-
 }
